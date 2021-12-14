798afb640317f52d04d6993d2e89a2dbb1bd3f06
Ruben <34570952+thepixelboy@users.noreply.github.com>
2021-12-14 18:53:34 +0100

    feat: chart to find the happiest dat of the week

---
+++ 6_happiest_day_of_the_week.py
@@ -0,0 +1,93 @@
+from datetime import datetime
+
+import justpy as jp
+import pandas
+
+data = pandas.read_csv("reviews.csv", parse_dates=["Timestamp"])
+data["Weekday"] = data["Timestamp"].dt.strftime("%A")
+data["Daynumber"] = data["Timestamp"].dt.strftime("%w")
+
+weekday_average = data.groupby(["Weekday", "Daynumber"]).mean()
+weekday_average = weekday_average.sort_values("Daynumber")
+
+chart_def = """
+{
+    chart: {
+        type: 'spline',
+        inverted: false
+    },
+    title: {
+        text: 'Aggregated Average Ratings by Day of the Week'
+    },
+    subtitle: {
+        text: 'According to the Course Reviews Dataset'
+    },
+    xAxis: {
+        reversed: false,
+        title: {
+            enabled: true,
+            text: 'Day'
+        },
+        labels: {
+            format: '{value}'
+        },
+        accessibility: {
+            rangeDescription: 'Range: 2018 to 2021.'
+        },
+        maxPadding: 0.05,
+        showLastLabel: true
+    },
+    yAxis: {
+        title: {
+            text: 'Rating'
+        },
+        labels: {
+            format: '{value}'
+        },
+        accessibility: {
+            rangeDescription: 'Range: 0 to 5.'
+        },
+        lineWidth: 2
+    },
+    legend: {
+        enabled: false
+    },
+    tooltip: {
+        headerFormat: '<b>{series.name}</b><br/>',
+        pointFormat: '{point.y}'
+    },
+    plotOptions: {
+        spline: {
+            marker: {
+                enable: false
+            }
+        }
+    },
+    series: [{
+        name: 'Rating',
+        data: [[0, 15], [10, -50], [20, -56.5], [30, -46.5], [40, -22.1],
+            [50, -2.5], [60, -27.7], [70, -55.7], [80, -76.5]]
+    }]
+}
+"""
+
+
+def app():
+    site = jp.QuasarPage()
+    h1 = jp.QDiv(
+        a=site,
+        text="Aggregated Average Ratings by Day of the Week",
+        classes="text-h3 text-center q-pa-md",
+    )
+    p1 = jp.QDiv(a=site, text="This graphs represent course review analysis")
+    hc = jp.HighCharts(a=site, options=chart_def)
+
+    hc.options.xAxis.categories = list(
+        weekday_average.index.get_level_values(0)
+    )
+    hc.options.series[0].data = list(weekday_average["Rating"])
+
+    return site
+
+
+jp.justpy(app)
